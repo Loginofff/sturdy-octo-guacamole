d4841ec8680f502a6056f70198e2fc7ff623de4d
Dmitrijs Loginovs <loginovs.dmitrijs@gmail.com>
2023-08-01 15:25:14 +0200

    Fix: compareTo

--- Collections, Comparable/code/src/Book.java
+++ Collections, Comparable/code/src/Book.java
@@ -1,50 +1,68 @@
+import java.util.Objects;
+
 public class Book implements Comparable<Book> {
 
-    private final String nameOfBook;
-    private final String author;
-    private final int quantity;
+  private final String nameOfBook;
+  private final String author;
+  private final int quantity;
 
-    public String getNameOfBook() {
-        return nameOfBook;
+  public String getNameOfBook() {
+    return nameOfBook;
+  }
+
+  public String getAuthor() {
+    return author;
+  }
+
+  public int getQuantity() {
+    return quantity;
+  }
+
+  public Book(String nameOfBook, String author, int quantity) {
+    this.nameOfBook = nameOfBook;
+    this.author = author;
+    this.quantity = quantity;
+  }
+
+  @Override
+  public String toString() {
+    return "Book{" +
+        "name='" + nameOfBook + '\'' +
+        ", author='" + author + '\'' +
+        ", quantity=" + quantity +
+        '}';
+  }
+
+  @Override
+  public int compareTo(Book o) {
+    int authorComparison = this.author.compareTo(o.author);
+    if (authorComparison != 0) {
+      return authorComparison;
     }
 
-    public String getAuthor() {
-        return author;
+    int nameComparison = this.nameOfBook.compareTo(o.nameOfBook);
+    if (nameComparison != 0) {
+      return nameComparison;
     }
 
-    public int getQuantity() {
-        return quantity;
+    return Integer.compare(this.quantity, o.quantity);
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
     }
-
-    public Book(String nameOfBook, String author, int pagesNum) {
-        this.nameOfBook = nameOfBook;
-        this.author = author;
-        this.quantity = pagesNum;
-
-
+    if (!(o instanceof Book other)) {
+      return false;
     }
+    return quantity == other.quantity &&
+        author.equals(other.author) &&
+        nameOfBook.equals(other.nameOfBook);
+  }
 
-    @Override
-    public String toString() {
-        return "Pizza{" +
-                "name='" + nameOfBook + '\'' +
-                ", author='" + author + '\'' +
-                ", pagesNum=" + quantity +
-                '}';
-    }
-
-    @Override
-    public int compareTo(Book o) {
-        int authorComparison = this.author.compareTo(o.author);
-        if (authorComparison != 0) {
-            return authorComparison;
-        }
-
-        int nameComparison = this.nameOfBook.compareTo(o.nameOfBook);
-        if (nameComparison != 0) {
-            return nameComparison;
-        }
-
-        return Integer.compare(this.quantity, o.quantity);
-    }
+  @Override
+  public int hashCode() {
+    return Objects.hash(nameOfBook, author, quantity);
+  }
 }

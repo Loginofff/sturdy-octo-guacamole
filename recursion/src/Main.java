2e94460d286c944e3ff98f6419257b3d4e9332b5
Dmitrijs Loginovs <loginovs.dmitrijs@gmail.com>
2023-08-21 18:32:42 +0200

    Fix: HomeWork_Recursion

--- recursion/src/Main.java
+++ recursion/src/Main.java
@@ -1,18 +1,23 @@
 public class Main {
 
-  //Напишите рекурсивный метод public static String reverse(int x),
+  // Напишите рекурсивный метод public static String reverse(int x),
   // который будет возвращать строку,
   // содержащую десятичные цифры числа в обратном порядке (развернёт число).
 
   public static void main(String[] args) {
-    reverse(1000);
+    int number = 12345;
+    String reversed = reverse(number);
+    System.out.println(reversed);
   }
 
-  private static void reverse(int n) {
-    if (n == 0)
-      return;
-    System.out.println(n);
-
-    reverse(n - 1);
+  private static String reverse(int x) {
+    if (x == 0) {
+      return "";
+    } else {
+      int lastDigit = x % 10;
+      String remainingReversed = reverse(x / 10);
+      return lastDigit + remainingReversed;
+    }
   }
 }
+
